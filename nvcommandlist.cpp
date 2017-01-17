cab9aa034d4df96daaf8ec1dd8eb9a848a5f3844
Christoph Kubisch <pixeljetstream@luxinia.de>
2017-01-17 16:25:23 +0100

    improve robustness for extension loader independent of actual GLEW version

--- nvcommandlist.cpp
+++ nvcommandlist.cpp
@@ -1,30 +1,38 @@
-/*-----------------------------------------------------------------------
-  Copyright (c) 2014, NVIDIA. All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions
-  are met:
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-   * Neither the name of its contributors may be used to endorse 
-     or promote products derived from this software without specific
-     prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
-  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
------------------------------------------------------------------------*/
+/* Copyright (c) 2014-2017, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "nvcommandlist.h"
 
+#ifdef GLEW_NV_command_list
+int init_NV_command_list( void (*fnGetProc(const char *name))() )
+{
+  return GLEW_NV_command_list != 0;
+}
+#else
 PFNGLCREATESTATESNVPROC __nvcCreateStatesNV;
 PFNGLDELETESTATESNVPROC __nvcDeleteStatesNV;
 PFNGLISSTATENVPROC __nvcIsStateNV;
@@ -46,7 +54,7 @@ PFNGLGETSTAGEINDEXNVPROC __nvcGetStageIndexNV;
 
 static int initedNVcommandList = 0;
 
-int init_NV_command_list(NVCPROC (*fnGetProc)(const char* name))
+int init_NV_command_list(void (*fnGetProc(const char *name))())
 {
   if (initedNVcommandList) return __nvcCreateStatesNV != ((void*)0);
 
@@ -72,4 +80,5 @@ int init_NV_command_list(NVCPROC (*fnGetProc)(const char* name))
   
   return __nvcCreateStatesNV != ((void*)0);
 }
+#endif
 
--- nvcommandlist.h
+++ nvcommandlist.h
@@ -1,41 +1,35 @@
-/*-----------------------------------------------------------------------
-  Copyright (c) 2014, NVIDIA. All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions
-  are met:
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-   * Neither the name of its contributors may be used to endorse 
-     or promote products derived from this software without specific
-     prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
-  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
------------------------------------------------------------------------*/
+/* Copyright (c) 2014-2017, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef NV_COMMANDLIST_H__
 #define NV_COMMANDLIST_H__
 
 #include <GL/glew.h>
 
-
-#  if defined(__MINGW32__) || defined(__CYGWIN__)
-#    define GLEXT_APIENTRY __stdcall
-#  elif (_MSC_VER >= 800) || defined(_STDCALL_SUPPORTED) || defined(__BORLANDC__)
-#    define GLEXT_APIENTRY __stdcall
-#  else
-#    define GLEXT_APIENTRY
-#  endif
 
 // bindless UBO
 
@@ -48,6 +42,130 @@
 #ifndef GL_UNIFORM_BUFFER_LENGTH_NV
 #define GL_UNIFORM_BUFFER_LENGTH_NV                         0x9370
 #endif
+
+typedef struct {
+  GLuint  header;
+} TerminateSequenceCommandNV;
+
+typedef struct {
+  GLuint  header;
+} NOPCommandNV;
+
+typedef  struct {
+  GLuint  header;
+  GLuint  count;
+  GLuint  firstIndex;
+  GLuint  baseVertex;
+} DrawElementsCommandNV;
+
+typedef  struct {
+  GLuint  header;
+  GLuint  count;
+  GLuint  first;
+} DrawArraysCommandNV;
+
+typedef  struct {
+  GLuint  header;
+  GLenum  mode;
+  GLuint  count;
+  GLuint  instanceCount;
+  GLuint  firstIndex;
+  GLuint  baseVertex;
+  GLuint  baseInstance;
+} DrawElementsInstancedCommandNV;
+
+typedef  struct {
+  GLuint  header;
+  GLenum  mode;
+  GLuint  count;
+  GLuint  instanceCount;
+  GLuint  first;
+  GLuint  baseInstance;
+} DrawArraysInstancedCommandNV;
+
+typedef struct {
+  GLuint  header;
+  GLuint  addressLo;
+  GLuint  addressHi;
+  GLuint  typeSizeInByte;
+} ElementAddressCommandNV;
+
+typedef struct {
+  GLuint  header;
+  GLuint  index;
+  GLuint  addressLo;
+  GLuint  addressHi;
+} AttributeAddressCommandNV;
+
+typedef struct {
+  GLuint    header;
+  GLushort  index;
+  GLushort  stage;
+  GLuint    addressLo;
+  GLuint    addressHi;
+} UniformAddressCommandNV;
+
+typedef struct {
+  GLuint  header;
+  float   red;
+  float   green;
+  float   blue;
+  float   alpha;
+} BlendColorCommandNV;
+
+typedef struct {
+  GLuint  header;
+  GLuint  frontStencilRef;
+  GLuint  backStencilRef;
+} StencilRefCommandNV;
+
+typedef struct {
+  GLuint  header;
+  float   lineWidth;
+} LineWidthCommandNV;
+
+typedef struct {
+  GLuint  header;
+  float   scale;
+  float   bias;
+} PolygonOffsetCommandNV;
+
+typedef struct {
+  GLuint  header;
+  float   alphaRef;
+} AlphaRefCommandNV;
+
+typedef struct {
+  GLuint  header;
+  GLuint  x;
+  GLuint  y;
+  GLuint  width;
+  GLuint  height;
+} ViewportCommandNV;  // only ViewportIndex 0
+
+typedef struct {
+  GLuint  header;
+  GLuint  x;
+  GLuint  y;
+  GLuint  width;
+  GLuint  height;
+} ScissorCommandNV;   // only ViewportIndex 0
+
+typedef struct {
+  GLuint  header;
+  GLuint  frontFace;  // 0 for CW, 1 for CCW
+} FrontFaceCommandNV;
+
+#ifndef GL_NV_command_list
+#define GL_NV_command_list 1
+
+#  if defined(__MINGW32__) || defined(__CYGWIN__)
+#    define GLEXT_APIENTRY __stdcall
+#  elif (_MSC_VER >= 800) || defined(_STDCALL_SUPPORTED) || defined(__BORLANDC__)
+#    define GLEXT_APIENTRY __stdcall
+#  else
+#    define GLEXT_APIENTRY
+#  endif
 
 #define GL_TERMINATE_SEQUENCE_COMMAND_NV                    0x0000
 #define GL_NOP_COMMAND_NV                                   0x0001
@@ -68,122 +186,6 @@
 #define GL_VIEWPORT_COMMAND_NV                              0x0010
 #define GL_SCISSOR_COMMAND_NV                               0x0011
 #define GL_FRONT_FACE_COMMAND_NV                            0x0012
-
-
-  typedef struct {
-    GLuint  header;
-  } TerminateSequenceCommandNV;
-
-  typedef struct {
-    GLuint  header;
-  } NOPCommandNV;
-
-  typedef  struct {
-    GLuint  header;
-    GLuint  count;
-    GLuint  firstIndex;
-    GLuint  baseVertex;
-  } DrawElementsCommandNV;
-
-  typedef  struct {
-    GLuint  header;
-    GLuint  count;
-    GLuint  first;
-  } DrawArraysCommandNV;
-
-  typedef  struct {
-    GLuint  header;
-    GLenum  mode;
-    GLuint  count;
-    GLuint  instanceCount;
-    GLuint  firstIndex;
-    GLuint  baseVertex;
-    GLuint  baseInstance;
-  } DrawElementsInstancedCommandNV;
-
-  typedef  struct {
-    GLuint  header;
-    GLenum  mode;
-    GLuint  count;
-    GLuint  instanceCount;
-    GLuint  first;
-    GLuint  baseInstance;
-  } DrawArraysInstancedCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    GLuint  addressLo;
-    GLuint  addressHi;
-    GLuint  typeSizeInByte;
-  } ElementAddressCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    GLuint  index;
-    GLuint  addressLo;
-    GLuint  addressHi;
-  } AttributeAddressCommandNV;
-
-  typedef struct {
-    GLuint    header;
-    GLushort  index;
-    GLushort  stage;
-    GLuint    addressLo;
-    GLuint    addressHi;
-  } UniformAddressCommandNV;
-
-  typedef struct {
-    GLuint    header;
-    float     red;
-    float     green;
-    float     blue;
-    float     alpha;
-  } BlendColorCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    GLuint  frontStencilRef;
-    GLuint  backStencilRef;
-  } StencilRefCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    float   lineWidth;
-  } LineWidthCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    float   scale;
-    float   bias;
-  } PolygonOffsetCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    float   alphaRef;
-  } AlphaRefCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    GLuint  x;
-    GLuint  y;
-    GLuint  width;
-    GLuint  height;
-  } ViewportCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    GLuint  x;
-    GLuint  y;
-    GLuint  width;
-    GLuint  height;
-  } ScissorCommandNV;
-
-  typedef struct {
-    GLuint  header;
-    GLuint  frontFace; // 0 for CW, 1 for CCW
-  } FrontFaceCommandNV;
-
-
 
 typedef void (GLEXT_APIENTRY *PFNGLCREATESTATESNVPROC)(GLsizei n, GLuint *states);
 typedef void (GLEXT_APIENTRY *PFNGLDELETESTATESNVPROC)(GLsizei n, const GLuint *states);
@@ -294,9 +296,9 @@ inline GLushort glGetStageIndexNV(GLenum shadertype)
 {
   return __nvcGetStageIndexNV(shadertype);
 }
+#endif
 
-typedef void (*NVCPROC)(void);
-int init_NV_command_list(NVCPROC (*fnGetProc)(const char* name));
+int init_NV_command_list(void (*fnGetProc(const char *name))());
 
 #endif
 
